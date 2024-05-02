d3e555c9060b46847f9c15c0407086223b6f0cc8
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-02 17:09:27 +0200

    feat: footer list printed and start to styled

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -4,7 +4,15 @@ export default {
         return {
             siteMap: [
                 {
-                    title: "COMICS",
+                    title: "DC COMICS",
+                    links: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"]
+                },
+                {
+                    title: "DC",
+                    links: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"]
+                },
+                {
+                    title: "SITES",
                     links: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"]
                 },
                 {
@@ -22,14 +30,65 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>SONO IL FOOTER</h1>
+    <div class="footer">
+        <ul class="footer_list">
+            <li v-for="(titles, index) in siteMap" class="foot_title">
+                {{ titles.title }}
+                <ul>
+                    <li v-for="curLink in siteMap[index].links" class="foot_text">
+                        <a href="">{{ curLink }}</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
         <div>
-            <ul>
-                <li v-for="titles in siteMap" class="foot_title">
-                    {{ titles.title }}
-                </li>
-            </ul>
+            <img src="../assets/img/dc-logo-bg.png" alt="dc logo transparent" class="logo">
         </div>
     </div>
 </template>
+
+<style scoped lang="scss">
+// IMPORT PARTIALS
+@use "../style/partials/_mixins" as *;
+@use "../style/partials/_variables" as *;
+
+.footer {
+    background-image: url(../assets/img/footer-bg.jpg);
+    height: 300px;
+    width: 100%;
+    @include flex (row, center, space-around);
+
+    .footer_list {
+        @include flex(row, center, center);
+        list-style-type: none;
+        gap: 1rem;
+
+        .foot_title {
+            font-size: 1rem;
+            font-weight: 800;
+            color: white;
+
+            .foot_text {
+                font-size: 0.8rem;
+                color: $font-color;
+                list-style-type: none;
+
+                a {
+                    color: $footer-text;
+                    text-decoration: none;
+                    font-weight: 400;
+
+                    &:hover {
+                        color: $active-color;
+                    }
+                }
+            }
+        }
+    }
+
+    .logo {
+        max-height: 300px;
+    }
+}
+</style>
